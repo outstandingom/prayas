25baf08c74519d82327e31f4604a596d3515aaa3
Om Uikey <ouikey41@gmail.com>
2026-06-17 08:01:31 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,10 @@
-import type { Database } from "./types";
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient<Database>(
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY =
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = createClient(
   SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY,
-  {
-    auth: {
-      storage: localStorage,
-      persistSession: true,
-      autoRefreshToken: true,
-    },
-  }
+  SUPABASE_PUBLISHABLE_KEY
 );
