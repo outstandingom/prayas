41404c174a8c287994ec6457ddb0835a5ab7a2a0
Om Uikey <ouikey41@gmail.com>
2026-06-23 10:23:16 +0530

    Remove DonatePopup and related logic from Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,25 +6,16 @@ import ScrollStory from '../components/ScrollStory'
 import ImpactCategories from '../components/ImpactCategories'
 import ImageCarousel from '../components/ImageCarousel'
 import VolunteerPopup from '../components/VolunteerPopup'
-import DonatePopup from '../components/DonatePopup'
 
 export default function Home() {
-  const [showDonatePopup, setShowDonatePopup] = useState(false)
   const [showVolunteerPopup, setShowVolunteerPopup] = useState(false)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowDonatePopup(true)
-    }, 2000)
+      setShowVolunteerPopup(true)
+    }, 2500) // Slightly longer delay since donate popup is removed
     return () => clearTimeout(timer)
   }, [])
-
-  const handleDonateClose = () => {
-    setShowDonatePopup(false)
-    setTimeout(() => {
-      setShowVolunteerPopup(true)
-    }, 500)
-  }
 
   return (
     <div className="w-full">
@@ -34,7 +25,6 @@ export default function Home() {
       <ImpactCategories />
       <ImageCarousel />
 
-      <DonatePopup isOpen={showDonatePopup} onClose={handleDonateClose} />
       <VolunteerPopup isOpen={showVolunteerPopup} onClose={() => setShowVolunteerPopup(false)} />
     </div>
   )
