3652d2b8dda7779bbe6e1129d69cbebaaf1fe66c
Om Uikey <ouikey41@gmail.com>
2026-07-03 15:26:56 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,8 +15,9 @@ export default function Home() {
     <div className="w-full">
       <HeroSection />
       <ScrollStory />
+       <ImpactCategories />
       <ImpactCounter />
-      <ImpactCategories />
+     
       <ImageCarousel />
 
 
