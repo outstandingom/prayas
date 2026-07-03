edfd83264c8613a0351ba02d0fa30771257da33c
Om Uikey <ouikey41@gmail.com>
2026-07-03 15:50:43 +0530

    Integrate TrustFlow component into Home page
    Added TrustFlow component to the Home page and adjusted imports.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,27 @@
 // src/pages/Home.tsx
 import { useState, useEffect } from 'react'
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'
 import HeroSection from '../components/HeroSection'
 import ImpactCounter from '../components/ImpactCounter'
 import ScrollStory from '../components/ScrollStory'
 import ImpactCategories from '../components/ImpactCategories'
 import ImageCarousel from '../components/ImageCarousel'
+import TrustFlow from '../components/TrustFlow'  // ← import the new component
 
 export default function Home() {
-  const { t } = useTranslation();
-
+  const { t } = useTranslation()
 
   return (
     <div className="w-full">
       <HeroSection />
       <ScrollStory />
-       <ImpactCategories />
+      <ImpactCategories />
       <ImpactCounter />
-     
+      
+      {/* Trust Flow section – shows the approach with GSAP animations */}
+      <TrustFlow />
+
       <ImageCarousel />
-
-
     </div>
   )
 }
